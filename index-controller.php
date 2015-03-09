bb078a0853695526053d0c752fd5fa5521041efe
Alonso Indacochea <aindaco1@hotmail.com>
2015-03-09 04:08:46 -0600

    updated many things

---
+++ index-controller.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once("Mail.php");
+
+try {
+	if(!@isset($_POST['name']) || !@isset($_POST['email']) || !@isset($_POST['message'])) {
+		throw new Exception('Form incomplete');
+	}
+
+	$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+	if($name === "") {
+		$name = "Shitbag";
+	}
+
+	$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+	$email = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+	$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+	if($message === "") {
+		$message = "Someone is trying to fuck with you guys.";
+	}
+
+	// email the user with an activation message
+	$to = "alonso@hermesdevelopment.com";
+	$from = $email;
+
+	// build headers
+	$headers = array();
+	$headers["To"] = $to;
+	$headers["From"] = $from;
+	$headers["Reply-To"] = $from;
+	$headers["Subject"] = "New Business";
+	$headers["MIME-Version"] = "1.0";
+	$headers["Content-Type"] = "text/html; charset=UTF-8";
+
+	$message = <<< EOF
+	<html>
+		<body>
+		<p>{$message}</p>
+		<br>
+		<p>{$name}</p>
+		</body>
+	</html>
+EOF;
+
+	// send the email
+	error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
+	$mailer =& Mail::factory("sendmail");
+	$status = $mailer->send($to, $headers, $message);
+	if(PEAR::isError($status) === true)
+	{
+		echo "<div class=\"alert alert-danger\" role=\"alert\">Failed to send message:" . $status->getMessage() . "</div>";
+	}
+	else
+	{
+		echo "<div class=\"alert alert-success\" role=\"alert\">Sent!</div>";
+	}
+} catch(Exception $exception) {
+	echo "<p class=\"alert alert-danger\" role=\"alert\">Exception: " . $exception->getMessage() . "</p>";
+}
--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,4 @@
-/********************************************
- * Custom JavaScript for Sample Parallax
- * DDC Student Server Class Materials
- * bootcamp-coders.cnm.edu/classmaterials
- *
- * @author rlewis37@cnm.edu
- *
- ********************************************/
-
-$(document).ready(function(){
+$(document).ready(function() {
 
 	/********************************************
 	 * Initialize stellar.js on window
@@ -30,4 +21,78 @@ $(document).ready(function(){
 		}
 	});
 
+	// tell the validator to validate this form (by id)
+	$("#controller").validate({
+		// setup the formatting for the errors
+		errorClass: "label-danger",
+		errorLabelContainer: "#outputArea",
+
+		// rules define what is good/bad input
+		rules: {
+			// each rule starts with the inputs name (NOT id)
+			name: {
+				maxlength: 100,
+				required: true
+			},
+
+			message: {
+				maxlength: 5000,
+				required: true
+			},
+
+			email: {
+				maxlength: 200,
+				email: true,
+				required: true
+			}
+
+		},
+
+		// error messages to display to the end user
+		messages: {
+			name: {
+				maxlength: "Your name is really longer than 100 characters? ",
+				required: "Please enter a name. "
+			},
+
+			message: {
+				maxlength: "Don't copy the text of \"Infinite Jest\", please. ",
+				required: "Please enter a message. "
+			},
+
+			email: {
+				maxlength: "Your email is really longer than 200 characters? ",
+				email: "Please enter a valid email address. ",
+				required: "Please enter an email address. "
+			}
+		},
+
+
+		submitHandler: function(form) {
+			$(form).ajaxSubmit({
+				// GET or POST
+				type: "POST",
+				// where to submit data
+				url: "../index-controller.php",
+				// TL; DR: reformat POST data
+				data: $(form),
+				// success is an event that happens when the server replies
+				success: function(ajaxOutput) {
+					// clear the output area's formatting
+					$("#outputArea").css("display", "block");
+					// write the server's reply to the output area
+					$("#outputArea").html(ajaxOutput);
+
+
+					// reset the form if it was successful
+					// this makes it easier to reuse the form again
+					if($(".alert-success").length >= 1) {
+						$(form)[0].reset();
+					}
+				}
+			});
+		}
+
+
+	});
 });
